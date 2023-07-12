cf524fc4dfc8b54493d5d22eb40cc072f0e2a79e
Giwa <giwatitilope899@gmail.com>
2023-07-12 21:01:03 +0100

    Modified the JavaScript file

--- pages/src/index.js
+++ pages/src/index.js
@@ -32,17 +32,3 @@ function showCity(event) {
 }
 let submitForm = document.querySelector("#search-input-form");
 submitForm.addEventListener("submit", showCity);
-/* Challenge 3 */
-function convertTempToCelsius() {
-  let convertToCelcius = document.querySelector("#temperature");
-  convertToCelcius.innerHTML = `⛅ 30`;
-}
-let tempC = document.querySelector("#celcius-link");
-tempC.addEventListener("click", convertTempToCelsius);
-
-function convertTempToFahrenheit() {
-  let convertToFahrenheit = document.querySelector("#temperature");
-  convertToFahrenheit.innerHTML = `⛅ 73`;
-}
-let tempF = document.querySelector("#fahrenheit-link");
-tempF.addEventListener("click", convertTempToFahrenheit);
