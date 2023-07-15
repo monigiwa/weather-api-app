7db786c35dacdabc1540aa45c6953d9f934b028d
Giwa <giwatitilope899@gmail.com>
2023-07-15 22:02:06 +0100

    Updated Js

--- pages/src/index.js
+++ pages/src/index.js
@@ -62,7 +62,6 @@ function showPosition(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(showTemperature);
-  // console.log(position.data.name);
 }
 function getCurrentLocation() {
   navigator.geolocation.getCurrentPosition(showPosition);
